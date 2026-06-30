2392c4114f9e6c227180103310d5f69a9371446f
zstar4004-star <zstar4004@gmail.com>
2026-07-01 02:09:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,20 @@
 // FILE: lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'canvas/editor_canvas.dart';
-import 'ui/toolbar.dart';
+import 'ui/editor_shell.dart';
 
 void main() {
-  runApp(const FigmaApp());
+  runApp(const App());
 }
 
-class FigmaApp extends StatelessWidget {
-  const FigmaApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: EditorShell(),
-    );
-  }
-}
-
-class EditorShell extends StatefulWidget {
-  const EditorShell({super.key});
-
-  @override
-  State<EditorShell> createState() => _EditorShellState();
-}
-
-class _EditorShellState extends State<EditorShell> {
-  String tool = "select";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          EditorCanvas(activeTool: tool),
-
-          Align(
-            alignment: Alignment.topCenter,
-            child: Toolbar(
-              onToolChanged: (t) {
-                setState(() => tool = t);
-              },
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
