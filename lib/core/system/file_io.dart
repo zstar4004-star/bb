1ccd54eb5fbe19824e52c07ef0ebbd51a556f457
zstar4004-star <zstar4004@gmail.com>
2026-07-01 02:06:30 +0300

    Create file_io.dart

---
+++ lib/core/system/file_io.dart
@@ -0,0 +1,17 @@
+// FILE: lib/core/system/file_io.dart
+
+import '../model/node.dart';
+
+class FileIO {
+  static Map<String, dynamic> export(List<Node> nodes) {
+    return {
+      "nodes": nodes.map((n) => {
+        "id": n.id,
+        "x": n.position.dx,
+        "y": n.position.dy,
+        "size": n.size,
+        "type": n.type.name,
+      }).toList()
+    };
+  }
+}
